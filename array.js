f70da50a82e1d93ca5adf2353a58f236bfc4ac45
Hiralal Chaudhary <dhairyachaudhary77@gmail.com>
2023-02-04 18:09:43 +0545

    about array function in js

---
+++ array.js
@@ -0,0 +1,34 @@
+let allStudents =[]
+singlestd = {
+    name: "student one",
+    emailAddress: "dhairyachaudhary77@gmail.com",
+    address: "hattiban",
+    ogibe: "9857055026"
+}
+singleStd = {
+    name: "student two",
+    email: "dhairyachaudhary77@gmail.com",
+    address: "hattiban",
+    ogibe: "9857055026"
+}
+singleStd = {
+    name: "student three",
+    email: "dhairyachaudhary77@gmail.com",
+    address: "hattiban",
+    ogibe: "9857055026"
+}
+
+allStd[0].name;
+
+let allStudents1 = [];
+let singleStd1 = {
+    name : "Sandesh Bhattarai",
+    email : "sandesh12@gmail.com",
+    address : "Kathmandu",
+    phone : "+977 9857055026"
+};
+// array populate
+allStudents.push(singleStd1);
+console.log(allStudents1);
+
+
